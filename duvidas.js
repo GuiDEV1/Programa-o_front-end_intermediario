8169a55c78cd348344126871f901922443486c59
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-05-23 21:55:51 -0300

    GuiDEV1/Programa-o_front-end_intermediario

---
+++ duvidas.js
@@ -0,0 +1,14 @@
+var uf =['SP', 'RJ', 'MG','ES','PR'];
+var txt='Hoje tem atividade!';
+var class_teste=document.querySelectorAll('.teste');
+
+document.querySelector("#nome").innerHTML= uf[1];
+document.querySelector('h2').innerHTML+='- '+uf.length;
+//document.querySelector('.teste').innerHTML= txt;
+//class_teste[3].innerHTML=txt;
+for(var i=0;i<class_teste.length;i++) {
+    class_teste[i].innerHTML=txt;
+}
+
+var imagem=["neve.jpg","floresta.jpg"];
+document.querySelector(".img").innerHTML="<img src='img/neve.jpg'>";
