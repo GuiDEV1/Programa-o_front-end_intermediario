3dfa0b83ecd51d898fdc3ac622aa644e32d952eb
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-06-06 21:08:31 -0300

    atividade

---
+++ atividade4.js
@@ -0,0 +1,3 @@
+var x= [[9,8,7,6],[5,4,3],[2,1]];
+
+document.querySelector("#container").innerHTML=x[0].length;
---
+++ revisão.js
@@ -0,0 +1,7 @@
+var x = [10,20,30,40,50];
+
+var y = [[3,4],[5,6,7],[8,9]];
+
+document.querySelector('#dv1').innerHTML = x[2];
+
+document.querySelector('.teste').innerHTML = y[1][2];
