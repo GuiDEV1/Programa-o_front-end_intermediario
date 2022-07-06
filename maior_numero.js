df1610bb54a981b12150e8fffd69bc07580cc74a
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-07-06 01:21:45 -0300

    Maior_n

--- maior_numero.js
+++ maior_numero.js
@@ -1,23 +1,21 @@
 /*
 Algoritimo que verifica o maior número e exibe para o usuario.
 autor: Guilherme M.Dias
-Versão 1.0.
+Versão 1.1.
+Data: 06 de julho de 2022.
 */
 
-var n1, n2, n3, n4, n5;
-alert('Digite 5 números e o programa ira exibir o maior!!')
-n1 = parseFloat( prompt('Digite um número: '));
-n2 = parseFloat( prompt('Digite um número: '));
-n3 = parseFloat( prompt('Digite um número: '));
-n4 = parseFloat( prompt('Digite um número: '));
-n5 = parseFloat( prompt('Digite um número: '));
+var n1, n2, n3, n4, n5,nt,t;
+ alert('Digite 5 números e o programa ira exibir o maior!!')
+ n1 = parseFloat( prompt('Digite um número: '));
+ n2 = parseFloat( prompt('Digite um número: '));
+ n3 = parseFloat( prompt('Digite um número: '));
+ n4 = parseFloat( prompt('Digite um número: '));
+ n5 = parseFloat( prompt('Digite um número: '));
 
-if ( n1 > n2 || n1 > n3 || n1 > n4 || n1 > n5) {
-    document.querySelector('#saida').innerHTML += 'O maior número foi ' + n1; 
-}
-else if (n2 > n1 || n2 > n3 || n2 > n4 || n2 > n5) {
-    document.querySelector('#saida').innerHTML += 'O maior número foi ' + n2; 
-}
-else if (n3 > n1 || n3 > n2 || n3 > n4 || n3 > n5 ) {
-     document.querySelector('#saida').innerHTML += 'O maior número foi ' + n3; 
-}
+nt = [n1,n2,n3,n4,n5]
+
+t = Math.max(...nt)
+
+
+document.querySelector('#saida').innerHTML += 'O maior valor Digitado foi de: ' +  t;
